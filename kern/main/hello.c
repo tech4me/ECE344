93986969dec7af6c99cd382ebd48465adf5c0a9a
yinshizh <yinshizh@51568464-c58e-413f-be39-8defd070d103>
2018-01-30 22:42:18 +0000

    Save working progress on read syscall
    git-svn-id: svn+ssh://ug250.eecg.utoronto.ca/srv/ece344s/os-071/svn/trunk@11 51568464-c58e-413f-be39-8defd070d103

--- kern/main/hello.c
+++ kern/main/hello.c
@@ -2,7 +2,11 @@
 #include <lib.h>
 #include <hello.h>
 
+#include <syscall.h>
+
 void hello()
 {
+    int ret;
+    sys_read(10, NULL, 0, ret);
     kprintf("Hello World\n");
 }
--- kern/userprog/syscall_impl.c
+++ kern/userprog/syscall_impl.c
@@ -1,10 +1,24 @@
 #include <types.h>
 #include <lib.h>
 #include <kern/errno.h>
+#include <kern/unistd.h>
 #include <syscall.h>
 
 int
 sys_read(int fd, void *buf, size_t buflen, int *retval)
 {
+    if (fd != STDIN_FILENO) {
+        kprintf("Right now read system call only support read from STDIN\n");
+        *retval = EUNIMP;
+        return -1;
+    }
+    if (buflen != 1) {
+        kprintf("Right now read system call only support read one character\n");
+        *retval = EUNIMP;
+        return -1;
+    }
+    char temp = getch();
+    *(char *)buf = temp;
+    kprintf("%c\n", temp);
     return 0;
 }
